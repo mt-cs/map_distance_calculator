6adc315a624ff3fe6c247c1c292e322074315ebd
mt-cs <Mtz31891usfca19>
2021-04-28 23:49:50 -0700

    it's midnight I am sleepy commit 😪

--- src/main/java/priorityQueue/MinHeap.java
+++ src/main/java/priorityQueue/MinHeap.java
@@ -3,9 +3,28 @@ package priorityQueue;
 /** A priority queue: represented by the min heap.
  *  Used in Prim's algorithm. */
 public class MinHeap {
-    private int[] heap; // the array to store the heap
-    private int maxsize; // the size of the array
-    private int size; // the current number of elements in the array
+    private elem[] heap; // the array to store the heap where each element has a node id and a cost.
+    private int maxsize; // the maximum size of the array
+    private int size; // the current size of the heap
+    private int[] positions; //where an index is a node id, and the value is its index in the heap array
+
+    private class elem {
+        private int nodeId;
+        private int cost;
+
+        public elem(int nodeIdInput, int costInput) {
+            nodeId = nodeIdInput;
+            cost = costInput;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+
+        public int getNodeId() {
+            return nodeId;
+        }
+    }
 
     /**
      * Constructor
@@ -13,9 +32,10 @@ public class MinHeap {
      */
     public MinHeap(int max) {
         maxsize = max;
-        heap = new int[maxsize];
+        heap = new elem[maxsize];
         size = 0;
-        heap[0] = Integer.MIN_VALUE;
+        heap[0] = new elem(Integer.MIN_VALUE, Integer.MIN_VALUE);
+
         // Note: no actual data is stored at heap[0].
         // Assigned MIN_VALUE so that it's easier to bubble up
     }
@@ -62,22 +82,23 @@ public class MinHeap {
      * @param pos2 the index of the second element in the heap
      */
     private void swap(int pos1, int pos2) {
-        int tmp;
+        elem tmp;
         tmp = heap[pos1];
         heap[pos1] = heap[pos2];
         heap[pos2] = tmp;
     }
 
-    /** Insert an element into the heap
+    /** Inserts node id with the given priority into the priority queue.
      *
-     * @param elem the element to insert
+     * @param nodeId the id of the node
+     * @param priority is the cost of the edge.
      */
-    public void insert(int elem) {
+    public void insert(int nodeId, int priority) {
         size++;
-        heap[size] = elem;
+        heap[size] = new elem(nodeId, priority);
 
         int current = size;
-        while (heap[current] < heap[parent(current)]) {
+        while (heap[current].getCost() < heap[parent(current)].getCost()) {
             swap(current, parent(current));
             current = parent(current);
         }
@@ -91,9 +112,19 @@ public class MinHeap {
         System.out.println();
     }
 
-    /** Remove minimum element (it is at the top of the minHeap)
+    /**
+     * Reduces the priority of the given vertex in the priority queue to newPriority,
+     * rearranging the queue (minHeap) as necessary.
+     * @param nodeId the id of the node
+     * @param newPriority new Priority
+     */
+    public void reduceKey(int nodeId, int newPriority) {
+
+    }
+
+    /** Removes the vertex with the smallest priority from the queue, and returns it.
      *
-     * @return the smallest element in the heap
+     * @return the smallest element in the priority queue
      */
     public int removeMin() {
         swap(1, size); // swap the end of the heap into the root
@@ -101,7 +132,7 @@ public class MinHeap {
         // fix the heap property - push down as needed
         if (size != 0)
             pushDown(1);
-        return heap[size + 1];
+        return heap[size + 1].getNodeId();
     }
 
     /** Push the value down the heap if it does not satisfy the heap property
@@ -112,12 +143,12 @@ public class MinHeap {
         int smallestchild;
         while (!isLeaf(position)) {
             smallestchild = leftChild(position); // set the smallest child to left child
-            if ((smallestchild < size) && (heap[smallestchild] > heap[smallestchild + 1]))
+            if ((smallestchild < size) && (heap[smallestchild].getNodeId() > heap[smallestchild + 1].getNodeId()))
                 smallestchild = smallestchild + 1; // right child was smaller, so smallest child = right child
 
             // the value of the smallest child is less than value of current,
             // the heap is already valid
-            if (heap[position] <= heap[smallestchild])
+            if (heap[position].getNodeId() <= heap[smallestchild].getNodeId())
                 return;
             swap(position, smallestchild);
             position = smallestchild;
